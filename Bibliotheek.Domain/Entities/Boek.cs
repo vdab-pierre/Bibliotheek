98adc10bbd0ee88a710e79a70db1f8bb876999f3
vdab-pierre @desktop <pierre.colin@vdab.be>
2013-09-16 15:48:55 +0200

    blah

--- Bibliotheek.Domain/Entities/Boek.cs
+++ Bibliotheek.Domain/Entities/Boek.cs
@@ -8,6 +8,7 @@ namespace Bibliotheek.Domain.Entities
 {
     public class Boek
     {
+        //blah
         public int Id { get; set; }
         public string Isbn { get; set; }
         public string Titel { get; set; }
@@ -15,5 +16,6 @@ namespace Bibliotheek.Domain.Entities
         public virtual ICollection<Auteur> Auteurs { get; set; }
         public virtual ICollection<Exemplaar> Exemplaren { get; set; }
         public virtual Uitgever Uitgever { get; set; }
+        
     }
 }
