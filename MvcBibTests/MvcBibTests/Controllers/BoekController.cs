e304319bbf101644a99add85e1ccf9f3bf70f3b8
vdab-pierre @desktop <pierre.colin@vdab.be>
2013-09-17 16:30:42 +0200

    Desktop

--- MvcBibTests/MvcBibTests/Controllers/BoekController.cs
+++ MvcBibTests/MvcBibTests/Controllers/BoekController.cs
@@ -95,6 +95,95 @@ namespace MvcBibTests.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        [HttpPost,ActionName("WisExemplaarVanBoek")]
+        public ActionResult WisExemplaarVanBoekConfirmed(int exId) {
+            var exemplaar = _db.Exemplaren.Find(exId);
+            if (exemplaar != null)
+            {
+                _db.Exemplaren.Remove(exemplaar);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Create() { 
+            //inputveld voor isbn weergeven
+            return View();
+        }
+
+        
+        
+        public ActionResult CreateOrLookUp(string isbn)
+        {
+            //boek bestaat in de db
+            // boek opzoeken
+            // boekinfo uit db weergeven, vragen hoeveel exemplaren toevoegen
+
+            // bestaat niet in db
+            // zoeken op het net
+            //   gevonden: boekinfo van website weergeven, vragen hoeveel exemplaar toevoegen
+
+            //   niet gevonden: lege boekvelden laten invullen, # exemplaren, toevoegen
+
+            if (string.IsNullOrEmpty(isbn))
+            {
+                return View("Create");
+            }
+            //correct isbn?
+            //else if() { 
+                
+            //}
+            var boek = _db.Boeken.Where(b => b.Isbn == isbn).FirstOrDefault();
+
+            if (boek != null)
+            {
+                return View("NewBoek", boek);
+            }
+            else { 
+                // op isbndb.org zoeken
+            }
+        
+        
+
+            return View();
+        }
+
+
+        public ActionResult CreateBoekOrExemplaar(Boek boek, int aantalEx) { 
+            //is het boek in de db dan aantal exemplaren toevoegen
+            //anders nieuw boek aanmaken
+            
+            if (boek != null) {
+                var boekInDb = _db.Boeken.Where(b => b.Isbn == boek.Isbn).FirstOrDefault();
+                if (boekInDb != null)
+                {
+                    //exemplaren toevoegen aan bestaand boek
+                    ExemplarenToevoegen(boekInDb, aantalEx);
+                    return RedirectToAction("Index", "Home");
+                }
+                else { 
+                    //nieuw boek maken en exemplaren eraan toevoegen
+                    if (ModelState.IsValid)
+                    {
+                        _db.Boeken.Add(boek);
+                        _db.SaveChanges();
+                        ExemplarenToevoegen(boek, aantalEx);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            return View();
+        }
+
+        private void ExemplarenToevoegen(Boek boek, int aantalEx) {
+            for (int i = 0; i < aantalEx; i++)
+            {
+                Exemplaar ex = new Exemplaar { Commentaar = "ex" + (i + 1)};
+                boek.Exemplaren.Add(ex);
+                _db.SaveChanges();
+            }
+        }
+
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);
