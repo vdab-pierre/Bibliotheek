6af4bd3afc447002ad2454949786806ca3beb284
vdab-pierre @desktop <pierre.colin@vdab.be>
2013-09-18 10:37:06 +0200

    Desktop, rename MvcBibTests -> MvcBib

---
+++ MvcBib/MvcBib/Controllers/BoekController.cs
@@ -0,0 +1,199 @@
+﻿using Bibliotheek.Domain.Concrete;
+using Bibliotheek.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBib.Controllers
+{
+    public class BoekController : Controller
+    {
+        private EFBibliotheekRepository _db;
+        
+        public BoekController()
+        {
+            _db = new EFBibliotheekRepository();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var boek = _db.Boeken.Find(id);
+            if (boek != null)
+            {
+                return View(boek);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Boek boek)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(boek).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(boek);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var boek = _db.Boeken.Where(b => b.Id == id).SingleOrDefault();
+            if (boek != null)
+            {
+                return View(boek);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult WisAlleExemplarenVanBoek(int id)
+        {
+            var boek = _db.Boeken.Find(id);
+            if (boek != null)
+            {
+                return View(boek);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost, ActionName("WisAlleExemplarenVanBoek")]
+        [ValidateAntiForgeryToken]
+        public ActionResult WisAlleExemplarenVanBoekConfirmed(int id) {
+            Boek boek = _db.Boeken.Find(id);
+            var exemplaren = boek.Exemplaren;
+            boek.Exemplaren = null;
+            foreach(var ex in exemplaren){
+                _db.Exemplaren.Remove(ex);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index","Home");
+        }
+
+        public ActionResult ExemplarenVanBoek(int id) {
+            Boek boek = _db.Boeken.Find(id);
+            return View(boek);
+        }
+
+        public ActionResult WisExemplaarVanBoek(int exId) {
+            var exemplaar = _db.Exemplaren.Find(exId);
+            if (exemplaar != null) {
+                return View(exemplaar);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost,ActionName("WisExemplaarVanBoek")]
+        public ActionResult WisExemplaarVanBoekConfirmed(int exId) {
+            var exemplaar = _db.Exemplaren.Find(exId);
+            if (exemplaar != null)
+            {
+                _db.Exemplaren.Remove(exemplaar);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Create() { 
+            //inputveld voor isbn weergeven
+            return View();
+        }
+
+        
+        
+        public ActionResult CreateOrLookUp(string isbn)
+        {
+            //boek bestaat in de db*
+            // boek opzoeken*
+            // boekinfo uit db weergeven, vragen hoeveel exemplaren en ze toevoegen*
+
+            // bestaat niet in db
+            // zoeken op het net
+            //   gevonden: boekinfo van website weergeven, vragen hoeveel exemplaar toevoegen
+
+            //   niet gevonden: lege boekvelden laten invullen, # exemplaren, toevoegen
+
+            if (string.IsNullOrEmpty(isbn))
+            {
+                return View("Create");
+            }
+            //correct isbn?
+            //else if() { 
+                
+            //}
+            var boek = _db.Boeken.Where(b => b.Isbn == isbn).FirstOrDefault();
+
+            if (boek != null)
+            {
+                return View("NewBoek", boek);
+            }
+            else { 
+                //boek bestaat niet in db
+                // op isbndb.org zoeken
+
+
+                //boek niet op het net gevonden
+            }
+        
+        
+
+            return View();
+        }
+
+
+        public ActionResult CreateBoekOrExemplaar(Boek boek, int aantalEx) { 
+            //is het boek in de db dan aantal exemplaren toevoegen
+            //anders nieuw boek aanmaken
+            
+            if (boek != null) {
+                var boekInDb = _db.Boeken.Where(b => b.Isbn == boek.Isbn).FirstOrDefault();
+                if (boekInDb != null)
+                {
+                    //exemplaren toevoegen aan bestaand boek
+                    ExemplarenToevoegen(boekInDb, aantalEx);
+                    return RedirectToAction("Index", "Home");
+                }
+                else { 
+                    //nieuw boek maken en exemplaren eraan toevoegen
+                    if (ModelState.IsValid)
+                    {
+                        _db.Boeken.Add(boek);
+                        _db.SaveChanges();
+                        ExemplarenToevoegen(boek, aantalEx);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            return View();
+        }
+
+        private void ExemplarenToevoegen(Boek boek, int aantalEx) {
+            for (int i = 0; i < aantalEx; i++)
+            {
+                Exemplaar ex = new Exemplaar { Commentaar = "ex" + (i + 1)};
+                boek.Exemplaren.Add(ex);
+                _db.SaveChanges();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing) {
+                _db.Dispose();
+            }
+        }
+    }
+}
---
+++ MvcBib/MvcBib/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Bibliotheek.Domain.Concrete;
+using Bibliotheek.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBib.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EFBibliotheekRepository _db;
+        public HomeController()
+        {
+            _db = new EFBibliotheekRepository();
+        }
+
+        public ActionResult TestTooltip() {
+            return View();
+        }
+
+        public ActionResult Index(string searchTerm=null)
+        {
+
+            var boeken = (from b in _db.Boeken
+                         from a in b.Auteurs
+                         where 
+                         searchTerm==null
+                         || a.Familienaam.StartsWith(searchTerm)
+                         || b.Titel.Contains(searchTerm)
+                         || b.Isbn.StartsWith(searchTerm)
+                         || b.Uitgever.Naam.StartsWith(searchTerm)
+                         || a.Voornaam.StartsWith(searchTerm)
+                         select b).ToList();
+
+            return View(boeken);
+        }
+
+        [HttpPost]
+        public ActionResult WisExemplaar(int boekId,int exId) {
+            var exemplaar = _db.Boeken.Find(boekId).Exemplaren.Where(ex=>ex.Id==exId).FirstOrDefault();
+            if (exemplaar == null)
+            {
+                return HttpNotFound();
+            }
+            _db.Exemplaren.Remove(exemplaar);
+            _db.SaveChanges();
+            return RedirectToAction("Index","Home");
+            //return View("Index", _db.Boeken.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+        }
+    }
+}
