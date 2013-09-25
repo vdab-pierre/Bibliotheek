6da3aa9e0aaa5f408bf08db9feabfd19db4bc3a6
vdab-pierre @desktop <pierre.colin@vdab.be>
2013-09-25 15:02:21 +0200

    Desktop

--- MvcBib/MvcBib/Controllers/AuteurController.cs
+++ MvcBib/MvcBib/Controllers/AuteurController.cs
@@ -4,6 +4,7 @@ using MvcBib.Models;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,20 +25,42 @@ namespace MvcBib.Controllers
             var boekInDb = _db.Boeken.Find(boekId);
             if (boekInDb != null){
                 var auteur = new Auteur();
-                auteur.Boeken = new HashSet<Boek>();
-                auteur.Boeken.Add(boekInDb);
                 
-                return View(auteur);
+                BoekAuteurViewModel vm = new BoekAuteurViewModel { Auteur=auteur,Boek=boekInDb,BoekId=boekId};
+
+                return View(vm);
             }
             throw new HttpException(404, "Boek niet gevonden");
             
         }
 
         [HttpPost]
-        public ActionResult CreateVoorBoek(Auteur auteur) {
+        public ActionResult CreateVoorBoek(BoekAuteurViewModel vm) {
             if (ModelState.IsValid) {
-                _db.Auteurs.Add(auteur);
-                _db.SaveChanges();
+                //eerst zoeken in db ...
+                var deAuteurInDb = _db.Auteurs.Where(a => a.Familienaam.Contains(vm.Auteur.Familienaam) && a.Voornaam.Contains(vm.Auteur.Voornaam)).FirstOrDefault();
+                var boekInDb = _db.Boeken.Find(vm.BoekId);
+                if (boekInDb != null)
+                {
+                    if (deAuteurInDb != null)
+                    {
+                        if (boekInDb.Auteurs == null)
+                        {
+                            boekInDb.Auteurs = new HashSet<Auteur>();
+                        }
+                        boekInDb.Auteurs.Add(deAuteurInDb);
+                        _db.SaveChanges();
+                    }
+                    else
+                    {
+                        boekInDb.Auteurs.Add(vm.Auteur);
+                        _db.SaveChanges();
+                    }
+                }
+                else {
+                    throw new HttpException(404, "Boek niet gevonden");
+                }
+                
             }
             return RedirectToAction("Index", "Home");
         }
---
+++ MvcBib/MvcBib/Models/BoekAuteurViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Bibliotheek.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcBib.Models
+{
+    public class BoekAuteurViewModel
+    {
+
+        public Boek Boek { get; set; }
+        public int BoekId { get; set; }
+        public Auteur Auteur { get; set; }
+    }
+}
